7ab045a8dbb9c466f8084259d66ff69371c68d8e
rushella <rushella.ln@outlook.com>
2025-02-10 23:21:02 +0500

    refactored project data layer structure

---
+++ LastFM.Analytics.Data/SqLiteDatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using AppAny.Quartz.EntityFrameworkCore.Migrations;
+using AppAny.Quartz.EntityFrameworkCore.Migrations.SQLite;
+using LastFM.Analytics.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LastFM.Analytics.Data;
+
+public sealed class SqLiteDatabaseContext(DbContextOptions<DatabaseContext> options)
+    : DatabaseContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>((entity) => 
+        {
+            entity.ToTable(nameof(User));
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => x.Name).IsUnique(true);
+        });
+
+        modelBuilder.AddQuartz(builder => builder.UseSqlite());
+    }
+}
