bcbb2c9f6843d5e9974cb725c2c080f152488ecd
rushella <rushella.ln@outlook.com>
2025-02-16 19:16:08 +0500

    simplified user entity, added sync status

--- LastFM.Analytics.Data/Entities/User.cs
+++ LastFM.Analytics.Data/Entities/User.cs
@@ -1,15 +1,16 @@
+using LastFM.Analytics.Data.Enums;
+
 namespace LastFM.Analytics.Data.Entities;
 
 public class User
 {
 	public long Id { get; set; }
 	public required string Name { get; set; }
-	public required long PlayCount { get; set; }
-	public required long ArtistCount { get; set; }
-	public required long TrackCount { get; set; }
-	public required long AlbumCount { get; set; }
+	
 	public string? Country { get; set; }
 	public string? ProfilePictureLinks { get; set; }
 	public required Uri Url { get;set; }
-	public required DateTime RegistrationDate { get; set; }
+	public DateTime? RegisteredAt { get; set; }
+	public DateTime? LastSyncedAt { get; set; }
+	public SyncStatus? SyncStatus { get; set; }
 }
