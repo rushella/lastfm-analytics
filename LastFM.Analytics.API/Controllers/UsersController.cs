71a65e5272c744312a7c8b2af6a9fd791d424ee6
rushella <rushella.ln@outlook.com>
2025-02-16 19:16:52 +0500

    added full-sync endpoint, deleted redundant controller

--- LastFM.Analytics.API/Controllers/UsersController.cs
+++ LastFM.Analytics.API/Controllers/UsersController.cs
@@ -1,13 +1,16 @@
+using LastFM.Analytics.API.Contracts.Enums;
+using LastFM.Analytics.API.SyncTasks;
 using LastFM.Analytics.Data;
 using LastFM.Analytics.Data.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Quartz;
 
 namespace LastFM.Analytics.API.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class UsersController(DatabaseContext databaseContext) : ControllerBase
+public class UsersController(DatabaseContext databaseContext, ISchedulerFactory schedulerFactory) : ControllerBase
 {
 	[HttpGet]
 	public async Task<ActionResult<User>> Get([FromQuery]string username)
@@ -18,6 +21,23 @@ public class UsersController(DatabaseContext databaseContext) : ControllerBase
 		{
 			return NotFound();
 		}
+		
+		return Ok(user);
+	}
+	
+	[HttpPost("/{username}/full-sync")]
+	public async Task<ActionResult<User>> Post(string username)
+	{
+		var user = await databaseContext.Users.Where((x) => x.Name == username).FirstOrDefaultAsync();
+		
+		if (user == null)
+		{
+			return NotFound();
+		}
+		
+		var scheduler = await schedulerFactory.GetScheduler();
+		
+		await scheduler.TriggerJob(new JobKey(nameof(FullSyncJob)), new JobDataMap { { "UserId", user.Id } });
 		
 		return Ok(user);
 	}
