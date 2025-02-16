73d586c92aa12a1b91f43785a898e0f879158028
rushella <rushella.ln@outlook.com>
2025-02-16 19:22:23 +0500

    refactored fullsyncjob a bit

--- LastFM.Analytics.API/SyncTasks/FullSyncJob.cs
+++ LastFM.Analytics.API/SyncTasks/FullSyncJob.cs
@@ -1,8 +1,6 @@
 ﻿using System.Text.Json;
 using IF.Lastfm.Core.Api;
 using LastFM.Analytics.Data;
-using LastFM.Analytics.Data.Entities;
-using Microsoft.EntityFrameworkCore;
 using Quartz;
 
 namespace LastFM.Analytics.API.SyncTasks;
@@ -11,43 +9,24 @@ public class FullSyncJob(DatabaseContext databaseContext, LastfmClient lastFmCli
 {
     public async Task Execute(IJobExecutionContext context)
     {
-        var userName = context.MergedJobDataMap["LastFmUserName"].ToString();
+        var userId = long.Parse(context.MergedJobDataMap["UserId"].ToString()!);
+        var user = await databaseContext.Users.FindAsync(userId, context.CancellationToken)!;
+
+        if (user == null)
+        {
+            throw new NotImplementedException();
+        }
         
-        var userInDatabase = await databaseContext.Users.SingleOrDefaultAsync(x => x.Name == userName, context.CancellationToken);
+        var lastFmResponse = await lastFmClient.User.GetInfoAsync(user.Name);
 
-        var userInfo = await lastFmClient.User.GetInfoAsync(userName);
-
-        if (!userInfo.Success)
+        if (!lastFmResponse.Success)
         {
             throw new NotImplementedException();
         }
 
-        if (userInDatabase == null)
-        {
-            var newUser = new User
-            {
-                AlbumCount = 0,
-                ArtistCount = 0,
-                PlayCount = userInfo.Content.Playcount,
-                Name = userInfo.Content.Name,
-                ProfilePictureLinks = JsonSerializer.Serialize(userInfo.Content.Avatar),
-                RegistrationDate = userInfo.Content.TimeRegistered,
-                TrackCount = 0,
-                Url = new Uri("https://last.fm/" + userInfo.Content.Name)
-            };
-            databaseContext.Add(newUser);
-        }
-        else
-        {
-            userInDatabase.AlbumCount = 0;
-            userInDatabase.ArtistCount = 0;
-            userInDatabase.PlayCount = userInfo.Content.Playcount;
-            userInDatabase.Name = userInfo.Content.Name;
-            userInDatabase.ProfilePictureLinks = JsonSerializer.Serialize(userInfo.Content.Avatar);
-            userInDatabase.RegistrationDate = userInfo.Content.TimeRegistered;
-            userInDatabase.TrackCount = 0;
-            userInDatabase.Url = new Uri("https://last.fm/" + userInfo.Content.Name);
-        }
+        user.Name = lastFmResponse.Content.Name;
+        user.ProfilePictureLinks = JsonSerializer.Serialize(lastFmResponse.Content.Avatar);
+        user.Url = new Uri("https://last.fm/" + lastFmResponse.Content.Name);
 
         await databaseContext.SaveChangesAsync(context.CancellationToken);
     }
